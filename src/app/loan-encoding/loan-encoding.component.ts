2368be8f6676f40b865754017ff2792a70cf6329
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-07-04 22:33:57 +0800

    Add functionality to FormControl

--- src/app/loan-encoding/loan-encoding.component.ts
+++ src/app/loan-encoding/loan-encoding.component.ts
@@ -3,35 +3,98 @@ import { NavigationComponent } from '../navigation/navigation.component';
 import { HeaderComponent } from '../header/header.component';
 import { UserService } from '../services/user/user.service';
 import { UserModel } from '../models/user.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { catchError, debounceTime, switchMap } from 'rxjs/operators';
+import { EMPTY, of } from 'rxjs';
 
 @Component({
   selector: 'app-loan-encoding',
   standalone: true,
-  imports: [NavigationComponent, HeaderComponent],
+  imports: [NavigationComponent, HeaderComponent, ReactiveFormsModule,],
   templateUrl: './loan-encoding.component.html',
   styleUrl: './loan-encoding.component.css'
 })
 export class LoanEncodingComponent implements OnInit {
-  users: UserModel[] = [];
-  user: UserModel | null = null;
-  private timeout: any;
+  debtorIdControl = new FormControl('');
+  debtorName = '';
+  debtorEmail = '';
 
-  constructor(private userService: UserService) {}
+  coborrowerIdControl = new FormControl('');
+  coborrowerName = '';
+  coborrowerEmail = '';
+
+  guarantorIdControl = new FormControl('');
+  guarantorName = '';
+  guarantorEmail = '';
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data.map((user: any) => ({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        id: user.id
-      }));
-    });
-  }
+    this.debtorIdControl.valueChanges
+      .pipe(
+        debounceTime(1000),
+        switchMap((id) => {
+          if (!id) return EMPTY;
+          return this.userService.getUser(+id).pipe(
+            catchError(() => of(null))
+          );
+        })
+      )
+      .subscribe({
+        next: (user: UserModel | null) => {
+          if (user) {
+            this.debtorName = `${user.firstName} ${user.lastName}`;
+            this.debtorEmail = user.email ?? '';
+          } else {
+            this.debtorName = 'Not Found';
+            this.debtorEmail = 'Not Found';
+          }
+        }
+      });
 
-  onSearchInput(value: string): void {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      this.user = this.users.find(u => u.id === value.trim()) || null;
-    }, 1000);
+      this.coborrowerIdControl.valueChanges.pipe(
+        debounceTime(1000), switchMap((id)=> {
+          if (!id) return EMPTY;
+          return this.userService.getUser(+id).pipe(
+            catchError(()=> of(null))
+          )
+        })
+      )
+      .subscribe({
+        next:(user:UserModel | null) => {
+          if (user) {
+            this.coborrowerName = `${user.firstName} ${user.lastName}`;
+            this.coborrowerEmail = user.email ?? '';
+          } else {
+            this.coborrowerName = 'Not Found';
+            this.coborrowerEmail = 'Not Found'
+          }
+        }
+      })
+
+
+      this.guarantorIdControl.valueChanges.pipe(
+        debounceTime(1000),
+        switchMap ((id) => {
+          if (!id) return EMPTY;
+          return this.userService.getUser(+id).pipe(
+            catchError(()=>of(null))
+          )
+        })
+      )
+      .subscribe({
+      next:(user:UserModel | null) => {
+        if (user) {
+          this.guarantorName = `${user.firstName} ${user.lastName}`;
+          this.guarantorEmail = user.email ?? '';
+        } else {
+          this.guarantorName = 'Not Found';
+          this.guarantorEmail = 'Not Found';
+        }
+      }
+      })
+
+
+
   }
 }
